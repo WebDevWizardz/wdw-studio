76272fcf37e09cbdecd0bb0ca0aa88a4fb542d9d
nathess <68251691+nathess@users.noreply.github.com>
2024-06-18 14:37:30 +0200

    react defaults clean up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center gap-y-12 h-screen ">
+      <h1 className="text-6xl">WDW studio 🪄</h1>
+      <img
+        src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExdmFkMGd0NWptMTB5NTlmMmw1aWFoajlwNnp6MG5sZHh6c3BkdjVkcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/eax0rh3OERAYg/giphy.gif"
+        alt=""
+        className="w-[400px]"
+      />
     </div>
   );
 }
