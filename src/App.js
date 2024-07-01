fb97dd153063d9f3449490e67406965b371511b1
nathess <68251691+nathess@users.noreply.github.com>
2024-07-01 15:39:35 +0200

    DM font added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Hero from "./Hero";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         alt=""
         className="w-[400px]"
       />
+      <Hero />
     </div>
   );
 }
