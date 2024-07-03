210d299f03a59bdc6d10000f03fc03768178dc40
nathess <68251691+nathess@users.noreply.github.com>
2024-07-03 12:35:14 +0200

    get in touch hover state

--- src/Landing/CTA.jsx
+++ src/Landing/CTA.jsx
@@ -3,9 +3,9 @@ import Arrow from "./Arrow";
 
 function CTA() {
   return (
-    <div className="">
+    <div>
       <div className="w-fit mt-[20px] md:mt-[20px] text-[40px] md:text-[60px] pl-[20px] md:pl-[60px] relative cursor-pointer">
-        <p className="shining-text">get in touch</p>
+        <p className="shining-text get-in-touch">get in touch</p>
         <span className="arrow-animation absolute">
           <Arrow />
         </span>
