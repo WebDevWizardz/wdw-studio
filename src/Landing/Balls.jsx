78d0e66b7e3f086c4820bbba1c9f506f0755d446
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-10-11 12:46:22 +0200

    debug

--- src/Landing/Balls.jsx
+++ src/Landing/Balls.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect } from "react"
 import Spline from "@splinetool/react-spline"
 
@@ -7,53 +6,20 @@ gsap.registerPlugin(CustomEase)
 
 function Balls() {
   CustomEase.create("ballsEase", "0.72,0,0.32,0.99")
-=======
-import React, { useEffect } from "react";
-import Spline from "@splinetool/react-spline";
-import { gsap, CustomEase } from "gsap/all";
-gsap.registerPlugin(CustomEase);
-
-// Custom hook useGSAP
-const useGSAP = (targets, mobileTargets) => {
->>>>>>> 6eaa5296ca0d92bc2ddd603225df91055da52e2a
   useEffect(() => {
-    CustomEase.create("ballsEase", "0.72,0,0.32,0.99");
-
-    const ballsAnimation = gsap.to(targets, {
+    gsap.to(".balls", {
       duration: 0.5,
       yPercent: -100,
       delay: 0.8,
       ease: "ballsEase",
-<<<<<<< HEAD
     })
     gsap.to(".balls-mobile", {
-=======
-    });
-
-    const ballsMobileAnimation = gsap.to(mobileTargets, {
->>>>>>> 6eaa5296ca0d92bc2ddd603225df91055da52e2a
       duration: 0.8,
       xPercent: -100,
       delay: 1.6,
       ease: "ballsEase",
-<<<<<<< HEAD
     })
   }, [])
-=======
-    });
-
-    // Cleanup to kill the animations when the component unmounts
-    return () => {
-      ballsAnimation.kill();
-      ballsMobileAnimation.kill();
-    };
-  }, [targets, mobileTargets]);
-};
-
-function Balls() {
-  // Using useGSAP hook
-  useGSAP(".balls", ".balls-mobile");
->>>>>>> 6eaa5296ca0d92bc2ddd603225df91055da52e2a
 
   return (
     <div className="md:col-span-1 h-[33%] md:h-screen relative">
